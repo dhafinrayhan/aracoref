89f5128c377e37739026d2e8c97d0cb8c673b6c6
Dhafin Rayhan A <dhafindiamle@gmail.com>
2023-07-25 07:02:33 +0700

    adjust bert setup

--- extract_bert_features/extract_bert_features.sh
+++ extract_bert_features/extract_bert_features.sh
@@ -1,2 +1,2 @@
-export BERT_MODEL_PATH="./araBERT_01/tf_arabert01"
-PYTHONPATH=. python extract_features.py --input_file="train.jsonlines;dev.jsonlines;test.jsonlines" --output_file=../bert_features.hdf5 --bert_config_file $BERT_MODEL_PATH/bert_config.json --init_checkpoint $BERT_MODEL_PATH/bert_model.ckpt --vocab_file  $BERT_MODEL_PATH/vocab.txt --do_lower_case=False --stride 1 --window_size 511
+export BERT_MODEL_PATH="./bert-large-arabertv02/tf-large-arabertv02"
+PYTHONPATH=. python extract_features.py --input_file="train.arabic.jsonlines;dev.arabic.jsonlines;test.arabic.jsonlines" --output_file=bert_large_arabertv02_features.hdf5 --bert_config_file ./bert-large-arabertv02/tf-large-arabertv02/config.json --init_checkpoint ./bert-large-arabertv02/tf-large-arabertv02/model.ckpt --vocab_file  ./bert-large-arabertv02/tf-large-arabertv02/vocab.txt --do_lower_case=False --stride 1 --window_size 511
